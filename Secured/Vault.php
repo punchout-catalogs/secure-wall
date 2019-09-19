f245c68fd1b259107082ff2e120d03d600463084
Tolik Legkyi <tolik.legkyi@punchoutcatalogs.com>
2019-09-19 12:23:15 +0300

    first commit

---
+++ Secured/Vault.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Punchout\Cloud\Security;
+
+class Vault
+{
+    protected static $config = null;
+    
+    protected function __construct()
+    {
+    }
+    
+    static public function getInstance(array $config = null)
+    {
+    }
+}
